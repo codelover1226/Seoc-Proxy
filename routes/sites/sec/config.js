485210bda99506578f1eaf919096c3f4266440e0
snow <160652025+codelover1226@users.noreply.github.com>
2024-08-19 08:49:53 +0900

    update swordt domain

--- routes/sites/sec/config.js
+++ routes/sites/sec/config.js
@@ -1,6 +1,6 @@
 module.exports = Object.create(Object.prototype, {
     domain: {
-        value: 'cru4.swordtserver.click', // Replace this value with the seocromom domain of your choice
+        value: 'cru3.swordtserver.click', // Replace this value with the seocromom domain of your choice
         writable: false
     },
 });
