2f7f3d09f0bafd6badcbc39ee3ad642725860438
snow <160652025+codelover1226@users.noreply.github.com>
2024-08-19 09:31:39 +0900

    update config file

--- routes/sites/sec/config.js
+++ routes/sites/sec/config.js
@@ -1,6 +1,6 @@
 module.exports = Object.create(Object.prototype, {
     domain: {
-        value: 'cru3.swordtserver.click', // Replace this value with the seocromom domain of your choice
+        value: 'seoc.metroserver.click', // Replace this value with the seocromom domain of your choice
         writable: false
     },
 });
