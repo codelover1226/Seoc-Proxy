2fdf5545ac5b4782e0ee44c0a20e0332a426995d
snow <160652025+codelover1226@users.noreply.github.com>
2024-08-20 11:29:39 +0900

    update domain

--- routes/sites/sec/config.js
+++ routes/sites/sec/config.js
@@ -1,6 +1,6 @@
 module.exports = Object.create(Object.prototype, {
     domain: {
-        value: 'seoc.metroserver.click', // Replace this value with the seocromom domain of your choice
+        value: 'cru.metroserver.click', // Replace this value with the seocromom domain of your choice
         writable: false
     },
 });
